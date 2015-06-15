7dedfa3abc9b39ef6071d9637848526c5b444ef2
zhouyue <zhouyue@zhouyuedeiMac.local>
2015-06-15 17:42:26 +0800

    use uisample

--- src/main/java/org/go3k/highlordconf/HelloWorldBuilder.java
+++ src/main/java/org/go3k/highlordconf/HelloWorldBuilder.java
@@ -11,12 +11,8 @@ import hudson.model.Descriptor;
 import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
 import hudson.tasks.Builder;
-import hudson.util.ListBoxModel;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import jenkins.model.Jenkins;
@@ -44,16 +40,18 @@ import org.kohsuke.stapler.StaplerRequest;
  */
 public class HelloWorldBuilder extends BuildWrapper {
     private ConnectType connectType;
-    private ConfigType fruit;
+    private Fruit fruit;
+//    private ConfigType fruit;
 //    private String configType;
 //    private String configFile;
 //    private String configValue;
 
     // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
-    public HelloWorldBuilder(ConnectType connectType, ConfigType fruit) {
+    public HelloWorldBuilder(ConnectType connectType, Fruit fruit) {
         this.connectType = connectType;
         this.fruit = fruit;
+//        this.fruit = fruit;
 //        this.configType = configType;
 //        this.configFile = configFile;
 //        this.configValue = configValue;
@@ -73,11 +71,20 @@ public class HelloWorldBuilder extends BuildWrapper {
 //    public String getConfigValue() {
 //        return configValue;
 //    }
-    public DescriptorExtensionList<ConfigType,Descriptor<ConfigType>> getConfigTypeDescriptors() {
-        return Jenkins.getInstance().<ConfigType,Descriptor<ConfigType>>getDescriptorList(ConfigType.class);
+//    public DescriptorExtensionList<ConfigType,Descriptor<ConfigType>> getConfigTypeDescriptors() {
+//        return Jenkins.getInstance().<ConfigType,Descriptor<ConfigType>>getDescriptorList(ConfigType.class);
+//    }
+//    public ConfigType getFruit() {
+//    	return fruit;
+//    }
+    
+    public Fruit getFruit() {
+        // Could return currently configured/saved item here to initialized form with this data
+        return fruit;
     }
-    public ConfigType getFruit() {
-    	return fruit;
+
+    public DescriptorExtensionList<Fruit,Descriptor<Fruit>> getFruitDescriptors() {
+        return Jenkins.getInstance().<Fruit,Descriptor<Fruit>>getDescriptorList(Fruit.class);
     }
 
     @Override
@@ -127,7 +134,8 @@ public class HelloWorldBuilder extends BuildWrapper {
             // ^Can also use req.bindJSON(this, formData);
             //  (easier when there are many fields; need set* methods for this, like setUseFrench)
             save();
-            return super.configure(req,formData);
+//            return super.configure(req,formData);
+            return true;
         }
         
         public String getConfigsFolder() {
@@ -135,19 +143,129 @@ public class HelloWorldBuilder extends BuildWrapper {
         }
     }
     
-    //Inner classes
-    public static abstract class ConfigType implements ExtensionPoint, Describable<ConfigType> {
+//    //Inner classes
+//    public static abstract class ConfigType implements ExtensionPoint, Describable<ConfigType> {
+//        protected String name;
+//        protected ConfigType(String name) { this.name = name; }
+//
+//        public ConfigTypeDescriptor getDescriptor() {
+//            return (ConfigTypeDescriptor)Jenkins.getInstance().getDescriptor(getClass());
+//        }
+//    }
+//
+//    public static class ConfigTypeDescriptor extends Descriptor<ConfigType> {
+//        public ConfigTypeDescriptor(Class<? extends ConfigType> clazz) {
+//            super(clazz);
+//        }
+//        public String getDisplayName() {
+//            return clazz.getSimpleName();
+//        }
+//        
+//        @Override
+//        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+//            save();
+//            return super.configure(req,formData);
+//        }
+//    }
+//
+//    public static class FileConfigType extends ConfigType {
+//        private String configFile;
+//        
+//        @DataBoundConstructor 
+//        public FileConfigType(String configFile) {
+//            super("选择配置文件");
+//            this.configFile = configFile;
+//        }
+//        
+//        public String getConfigFile() {
+//            return configFile;
+//        }
+//        public String getDefaultConfigValue() {
+//        	String confFolder = Helper.Instance().getMainBuilder().getDescriptor().getConfigsFolder();
+//        	String filename = confFolder + (confFolder.endsWith("/") ? "" : "/") + configFile;
+//        	if (filename.length() > 0)
+//        	{
+////        		return filename;
+//        		File file = new File(filename);
+//        		return Helper.Read2String(file);
+//        	}
+//            return "Hello. hello";
+//        }
+//        
+//        
+////        @Extension public static final ConfigTypeDescriptor D = new ConfigTypeDescriptor(FileConfigType.class);
+//        
+//        public FileConfigTypeDescriptor getDescriptor() {
+//            return (FileConfigTypeDescriptor)super.getDescriptor();
+//        }
+//        @Extension
+//        public static final class FileConfigTypeDescriptor extends ConfigTypeDescriptor {
+//        	private List<String> allConfigFiles = null;
+//        	
+//        	public FileConfigTypeDescriptor() {
+//        		super(FileConfigType.class);
+//            }
+//            
+//            private List<String> GetAllConfigFiles()
+//            {
+//            	if (allConfigFiles == null)
+//            	{
+//            		String configsFolder = Helper.Instance().getMainBuilder().getDescriptor().getConfigsFolder();
+//            		
+//            		allConfigFiles = new ArrayList<String>();
+//                	File dir = new File(configsFolder);
+//                    File file[] = dir.listFiles();
+//                    for (int i = 0; i < file.length; i++) {
+//                    	if (!file[i].isFile()) continue;
+//                    	
+//                    	String name = file[i].getName();
+//                    	if (name.startsWith(".")) continue;
+//                    	allConfigFiles.add(name);
+//                    }
+//            	}
+//            	
+//            	return allConfigFiles;
+//            }
+//
+//            public ListBoxModel doFillConfigFileItems() {
+//                ListBoxModel items = new ListBoxModel();
+//                for (String file : GetAllConfigFiles()) {
+//                    items.add(file, file);
+//                }
+//                return items;
+//            }
+//            
+//            @Override
+//            public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+//                save();
+//                return super.configure(req,formData);
+//            }
+//        }
+//    }
+//
+//    public static class CustomConfigType extends ConfigType {
+//        private String custom;
+//        @DataBoundConstructor 
+//        public CustomConfigType(String custom) {
+//            super("CustomConfigType");
+//            this.custom = custom;
+//        }
+//        @Extension public static final ConfigTypeDescriptor D = new ConfigTypeDescriptor(CustomConfigType.class);
+//    }
+    
+    public static abstract class Fruit implements ExtensionPoint, Describable<Fruit> {
         protected String name;
-        protected ConfigType(String name) { this.name = name; }
+        protected Fruit(String name) { this.name = name; }
 
-        public ConfigTypeDescriptor getDescriptor() {
-            return (ConfigTypeDescriptor)Jenkins.getInstance().getDescriptor(getClass());
+        public Descriptor<Fruit> getDescriptor() {
+            return Jenkins.getInstance().getDescriptor(getClass());
         }
     }
 
-    public static class ConfigTypeDescriptor extends Descriptor<ConfigType> {
-        public ConfigTypeDescriptor(Class<? extends ConfigType> clazz) {
+    public static class FruitDescriptor extends Descriptor<Fruit> {
+        public FruitDescriptor(Class<? extends Fruit> clazz) {
             super(clazz);
+            load();
         }
         public String getDisplayName() {
             return clazz.getSimpleName();
@@ -156,94 +274,27 @@ public class HelloWorldBuilder extends BuildWrapper {
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
             save();
-            return super.configure(req,formData);
+//            return super.configure(req,formData);
+            return true;
         }
     }
 
-    public static class FileConfigType extends ConfigType {
-        private String configFile;
-        
-        @DataBoundConstructor 
-        public FileConfigType(String configFile) {
-            super("选择配置文件");
-            this.configFile = configFile;
+    public static class Apple extends Fruit {
+        private int seeds;
+        @DataBoundConstructor public Apple(int seeds) {
+            super("Apple");
+            this.seeds = seeds;
         }
-        
-        public String getConfigFile() {
-            return configFile;
-        }
-        public String getDefaultConfigValue() {
-        	String confFolder = Helper.Instance().getMainBuilder().getDescriptor().getConfigsFolder();
-        	String filename = confFolder + (confFolder.endsWith("/") ? "" : "/") + configFile;
-        	if (filename.length() > 0)
-        	{
-//        		return filename;
-        		File file = new File(filename);
-        		return Helper.Read2String(file);
-        	}
-            return "Hello. hello";
-        }
-        
-        
-//        @Extension public static final ConfigTypeDescriptor D = new ConfigTypeDescriptor(FileConfigType.class);
-        
-        public FileConfigTypeDescriptor getDescriptor() {
-            return (FileConfigTypeDescriptor)super.getDescriptor();
-        }
-        @Extension
-        public static final class FileConfigTypeDescriptor extends ConfigTypeDescriptor {
-        	private List<String> allConfigFiles = null;
-        	
-        	public FileConfigTypeDescriptor() {
-        		super(FileConfigType.class);
-            }
-            
-            private List<String> GetAllConfigFiles()
-            {
-            	if (allConfigFiles == null)
-            	{
-            		String configsFolder = Helper.Instance().getMainBuilder().getDescriptor().getConfigsFolder();
-            		
-            		allConfigFiles = new ArrayList<String>();
-                	File dir = new File(configsFolder);
-                    File file[] = dir.listFiles();
-                    for (int i = 0; i < file.length; i++) {
-                    	if (!file[i].isFile()) continue;
-                    	
-                    	String name = file[i].getName();
-                    	if (name.startsWith(".")) continue;
-                    	allConfigFiles.add(name);
-                    }
-            	}
-            	
-            	return allConfigFiles;
-            }
-
-            public ListBoxModel doFillConfigFileItems() {
-                ListBoxModel items = new ListBoxModel();
-                for (String file : GetAllConfigFiles()) {
-                    items.add(file, file);
-                }
-                return items;
-            }
-            
-            @Override
-            public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-                save();
-                return super.configure(req,formData);
-            }
-        }
+        @Extension public static final FruitDescriptor D = new FruitDescriptor(Apple.class);
     }
 
-    public static class CustomConfigType extends ConfigType {
-        private String custom;
-        @DataBoundConstructor 
-        public CustomConfigType(String custom) {
-            super("CustomConfigType");
-            this.custom = custom;
+    public static class Banana extends Fruit {
+        private boolean yellow;
+        @DataBoundConstructor public Banana(boolean yellow) {
+            super("Banana");
+            this.yellow = yellow;
         }
-        @Extension public static final ConfigTypeDescriptor D = new ConfigTypeDescriptor(CustomConfigType.class);
+        @Extension public static final FruitDescriptor D = new FruitDescriptor(Banana.class);
     }
-    
 }
 
